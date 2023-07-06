f8cd9cc5cf8758e3b3a1eb3997c9311c646866ef
Seb Tiburzio <sebtiburzio@gmail.com>
2023-07-06 16:28:29 +0200

    Static solver testing

--- floating_base/plot_config.m
+++ floating_base/plot_config.m
@@ -8,11 +8,13 @@ for i_s = 1:length(s_vect)
     xy_c(i_s,:) = fk_fcn(p_vals, q, s_vect(i_s), 0);
 end
 
-plot(xy_c(:,1),xy_c(:,2), Color=[1.0 1.0-alpha*0.5 1.0-alpha*1.0], LineWidth=3)
+plot([xy_c(1,1),xy_c(1,1)+0.05*sin(q(5))],[xy_c(1,2),xy_c(1,2)+0.05*cos(q(5))],'k',LineWidth=3)
 hold on
+plot([0,0],[0,0.333],'k', LineWidth=3)
+plot(xy_c(:,1),xy_c(:,2), Color=[1.0 1.0-alpha*0.5 1.0-alpha*1.0], LineWidth=3)
 scatter(xy_c(11,1),xy_c(11,2), 20, [0.4660 0.6740 0.1880], 'filled')
 scatter(xy_c(end,1),xy_c(end,2), 20, [0 0.4470 0.7410], 'filled')
-xlim([-p_vals(3)-0.1,p_vals(3)+0.1])
+xlim([xy_c(1,1)-p_vals(3)-0.1,xy_c(1,1)+p_vals(3)+0.1])
 axis equal
 grid on
 hold off
--- floating_base/solve_static.m
+++ floating_base/solve_static.m
@@ -9,20 +9,56 @@ k = 0.01;
 Theta_bar = [-0.5,3];
 
 global goal
-q_0 = [1e-3; 1e-3; 0; 0; 0];
+q_0 = [1e-3; 1e-3; 0.0; 0.0; 0.0];
 
-lb = [-Inf,-Inf, -1, 0, -3*pi/4]; % Theta0, Theta1, X, Z, Phi
-ub = [Inf, Inf, 1, 1, 3*pi/4];
+lb = [-Inf,-Inf, -0.4, 0.333, -3*pi/4]; % Theta0, Theta1, X, Z, Phi
+ub = [Inf, Inf, 0.855, 1.19, pi/4];
 
 %%
 % Position only
-goal = [0.5; 0.5];
-q_st = fmincon(@f,q_0,[],[],[],[],lb,ub,@static_model)
+goal = [0.5; 0.3];
+q_st = fmincon(@f,q_0,[],[],[],[],lb,ub,@nonlcon)
 
 %%
 % Position and orientation
-goal = [0.5; 0.5; pi/2];
-q_st = fmincon(@fa,q_0,[],[],[],[],lb,ub,@static_model)
+goal = [0.8; 0.3; pi/2];
+q_st = fmincon(@fa,q_0,[],[],[],[],lb,ub,@nonlcon)
+
+%%
+%Line
+q_0 = [1e-3; 1e-3; 0.0; 0.0; 0.0];
+for i = 0:9
+    goal = [0.1+0.09*i; 0.1];
+%     goal = [0.4; 0.1+0.07*i];
+    q_st = fmincon(@f,q_0,[],[],[],[],lb,ub,@nonlcon)
+    q_0 = q_st;
+    plot_config(q_st,i/10+0.1)
+    hold on
+end
+hold off
+
+%%
+%Circle
+q_0 = [1e-3; 1e-3; 0.0; 0.0; 0.0];
+for i = 0:pi/6:2*pi
+    goal = [0.5+0.1*cos(i); 0.6-0.1*sin(i)];
+    q_st = fmincon(@f,q_0,[],[],[],[],lb,ub,@nonlcon)
+    q_0 = q_st;
+    plot_config(q_st,i/6.5+0.03)
+    hold on
+end
+hold off
+
+%%
+% Plot constraints
+hold on
+xline([lb(3) ub(3)],'r')
+yline([lb(4) lb(4)],'r')
+th = 0:pi/50:2*pi;
+xunit = 0.85 * cos(th);
+yunit = 0.85 * sin(th) + 0.333;
+h = plot(xunit, yunit,'r');
+hold off
 
 %%
 % Objective function - minimise endpoint goal distance
@@ -37,12 +73,14 @@ function obj = fa(q)
     obj = norm(fka_fcn(p_vals, q, 1, 0) - goal);
 end
 
-% Constraint - steady state of object
-function [c,ceq] = static_model(q)
+% Constraints
+function [c,ceq] = nonlcon(q)
+    % Steady state of object
     global p_vals k Theta_bar
     G_eval = G_fcn(p_vals,q);
     ceq = G_eval(1:2) + k.*[q(1)-Theta_bar(1); q(2)-Theta_bar(2)];
-    c = [];
+    % Circular workspace region
+    c = q(3)^2 + (q(4)-0.333)^2 - 0.85^2;
 end
 
 

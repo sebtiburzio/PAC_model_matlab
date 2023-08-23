cafcafd01301a3d4e5e24739492de7688fc52cca
Seb Tiburzio <sebtiburzio@gmail.com>
2023-08-23 17:18:34 +0200

    Redo dynamic parameter ID without BCScale

--- fixed_base/param_ID.m
+++ fixed_base/param_ID.m
@@ -1,5 +1,5 @@
 clear
-addpath('automatically_generated/fixed')
+addpath('automatically_generated')
 
 %%
 % Load predefined object parameters
@@ -7,7 +7,7 @@ load('../object_parameters/black_short_weighted.mat')
 
 %%
 % Load data
-data = readmatrix("data_in/0801/black_short_unweighted_swing/theta_evolution.csv");
+data = readmatrix("data_in/0801-swing_data/black_short_weighted_swing/theta_evolution.csv");
 ts = data(:,1)';
 Theta0 = data(:,2)';
 Theta1 = data(:,3)';
@@ -32,16 +32,16 @@ c01 = dTheta0 + dTheta1/2.0;
 c10 = Theta0/2.0 + Theta1/3.0;
 c11 = dTheta0/2.0 + dTheta1/3.0;
 
-%% Identify B/C Scale and Beta
+%% Identify Beta
 
 for sample = 1:num_samples
     
-    RHS = -G_fcn(p_vals, Theta(:,sample)) - k_obj*[1 1/2; 1/2 1/3]*(Theta(:,sample)-Theta_bar);
+    RHS = -B_fcn(p_vals,Theta(:,sample))*ddTheta(:,sample) ...
+          -C_fcn(p_vals,Theta(:,sample),dTheta(:,sample))*dTheta(:,sample) ...
+          -G_fcn(p_vals, Theta(:,sample)) ...
+          -k_obj*[1 1/2; 1/2 1/3]*(Theta(:,sample)-Theta_bar);
 
-    Y_n = [
-            B_fcn(p_vals,Theta(:,sample))*ddTheta(:,sample) + C_fcn(p_vals,Theta(:,sample),dTheta(:,sample))*dTheta(:,sample), ...
-            [c0(sample); c1(sample)] 
-          ];
+    Y_n = [c0(sample); c1(sample)];
     
     if sample == 1
         delta = RHS;
@@ -54,11 +54,9 @@ for sample = 1:num_samples
 end
 
 Pi = pinv(Y)*delta;
-BC_Scale = Pi(1)
-beta_obj = Pi(2)
 
 %% Save parameters
-save('black_short_weighted', 'p_vals', 'k_obj', 'Theta_bar', 'BC_Scale', 'beta_obj')
+save('orange_short_weighted', 'p_vals', 'k_obj', 'Theta_bar', 'beta_obj')
 
 %% Identify stiffness and damping
 % # Calc delta vector
